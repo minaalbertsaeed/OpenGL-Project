5a9c90be076fda86f10ed0b650ee4a5493cdcec9
minaalbert33 <mina.albert33@gmail.com>
2024-05-16 07:59:07 +0300

    Move Drawing utilties into seperate .h & .cpp

---
+++ src/DrawingUtilities.cpp
@@ -0,0 +1,56 @@
+#include <GL/glut.h>
+#include "globalVariables.h"
+#include "DrawingUtilities.h"
+
+void drawCube(GLfloat  x, GLfloat y, GLfloat  z){
+
+
+   glLoadIdentity();                 
+   glTranslatef(x, y, z);  
+   glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  
+   glBegin(GL_QUADS);                
+
+      glColor3f(0.0f, 1.0f, 0.0f);     // Green
+      glVertex3f( 1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f, -1.0f);
+      glVertex3f(-1.0f, 1.0f,  1.0f);
+      glVertex3f( 1.0f, 1.0f,  1.0f);
+ 
+
+      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
+      glVertex3f( 1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+ 
+
+      glColor3f(1.0f, 0.0f, 0.0f);     // Red
+      glVertex3f( 1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f,  1.0f, 1.0f);
+      glVertex3f(-1.0f, -1.0f, 1.0f);
+      glVertex3f( 1.0f, -1.0f, 1.0f);
+ 
+
+      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
+      glVertex3f( 1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f( 1.0f,  1.0f, -1.0f);
+ 
+
+      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
+      glVertex3f(-1.0f,  1.0f,  1.0f);
+      glVertex3f(-1.0f,  1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f, -1.0f);
+      glVertex3f(-1.0f, -1.0f,  1.0f);
+ 
+
+      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
+      glVertex3f(1.0f,  1.0f, -1.0f);
+      glVertex3f(1.0f,  1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f,  1.0f);
+      glVertex3f(1.0f, -1.0f, -1.0f);
+   glEnd();  
+
+
+}
---
+++ src/DrawingUtilities.h
@@ -0,0 +1,9 @@
+#ifndef DrawingUtilities
+#define DrawingUtilities
+
+#include <GL/glut.h>
+#include "globalVariables.h"
+
+void drawCube(GLfloat  x, GLfloat y, GLfloat  z);
+
+#endif
