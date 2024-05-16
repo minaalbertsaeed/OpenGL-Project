132875dbddb3b07238ab811fa17a628e92cae02f
minaalbert33 <mina.albert33@gmail.com>
2024-05-16 13:52:10 +0300

    Edit drawCube to be more generic

--- src/DrawingUtilities.cpp
+++ src/DrawingUtilities.cpp
@@ -1,54 +1,66 @@
+#include <GL/gl.h>
 #include <GL/glut.h>
+#include <iostream>
+#include <vector>
 #include "globalVariables.h"
 #include "DrawingUtilities.h"
 
-void drawCube(GLfloat  x, GLfloat y, GLfloat  z){
-   // glLoadIdentity();                 
-   // glTranslatef(x, y, z);  
-   // glRotatef(angleCube, 1.0f, 1.0f, 1.0f);  
-   glBegin(GL_QUADS);                
+void drawCube(GLfloat height, GLfloat width, GLfloat depth, GLfloat xc, GLfloat yc, GLfloat zc){
+     VectorOfVertices vertices = {
+            {(xc - 0.5f * width), (yc + 0.5f * height), zc}, // Top-Left (0)
+            {(xc + 0.5f * width), (yc + 0.5f * height), zc}, // Top-Right (1
+            {(xc + 0.5f * width), (yc - 0.5f * height), zc}, // Bottom-Right (2 
+            {(xc - 0.5f * width), (yc - 0.5f * height), zc}, // Bottom-Left 3 
 
-      glColor3f(0.0f, 1.0f, 0.0f);     // Green
-      glVertex3f( 1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f, -1.0f);
-      glVertex3f(-1.0f, 1.0f,  1.0f);
-      glVertex3f( 1.0f, 1.0f,  1.0f);
- 
+            {(xc - 0.5f * width), (yc + 0.5f * height), zc - depth}, // Top-Left 4 
+            {(xc + 0.5f * width), (yc + 0.5f * height), zc - depth}, // Top-Right 5 
+            {(xc + 0.5f * width), (yc - 0.5f * height), zc - depth}, // Bottom-Right 6 
+            {(xc - 0.5f * width), (yc - 0.5f * height), zc - depth}, // Bottom-Left 7 
 
-      glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-      glVertex3f( 1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f( 1.0f, -1.0f, -1.0f);
- 
+    };
 
-      glColor3f(1.0f, 0.0f, 0.0f);     // Red
-      glVertex3f( 1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f,  1.0f, 1.0f);
-      glVertex3f(-1.0f, -1.0f, 1.0f);
-      glVertex3f( 1.0f, -1.0f, 1.0f);
- 
+    glPointSize(10);
 
-      glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-      glVertex3f( 1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f( 1.0f,  1.0f, -1.0f);
- 
+    glBegin(GL_POINTS);
+        glColor3f(1.0f, 1.0f, 1.0f);     // Green
+        glVertex3f(xc,yc,zc);
+    glEnd();
 
-      glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-      glVertex3f(-1.0f,  1.0f,  1.0f);
-      glVertex3f(-1.0f,  1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f, -1.0f);
-      glVertex3f(-1.0f, -1.0f,  1.0f);
- 
+    glBegin(GL_QUADS);
+        glColor3f(1.0f, 0.0f, 0.0f);     // Green
+        glVertex3f(vertices[0][0], vertices[0][1], vertices[0][2]);
+        glVertex3f(vertices[1][0], vertices[1][1], vertices[1][2]);
+        glVertex3f(vertices[2][0], vertices[2][1], vertices[2][2]);
+        glVertex3f(vertices[3][0], vertices[3][1], vertices[3][2]);
 
-      glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-      glVertex3f(1.0f,  1.0f, -1.0f);
-      glVertex3f(1.0f,  1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f,  1.0f);
-      glVertex3f(1.0f, -1.0f, -1.0f);
-   glEnd();  
+        glColor3f(0.0f, 1.0f, 0.0f);     // Green
+        glVertex3f(vertices[0][0], vertices[0][1], vertices[0][2]);
+        glVertex3f(vertices[4][0], vertices[4][1], vertices[4][2]);
+        glVertex3f(vertices[5][0], vertices[5][1], vertices[5][2]);
+        glVertex3f(vertices[1][0], vertices[1][1], vertices[1][2]);
 
-
+        glColor3f(0.0f, 1.0f, 0.0f);     // Green
+        glVertex3f(vertices[1][0], vertices[1][1], vertices[1][2]);
+        glVertex3f(vertices[5][0], vertices[5][1], vertices[5][2]);
+        glVertex3f(vertices[6][0], vertices[6][1], vertices[6][2]);
+        glVertex3f(vertices[2][0], vertices[2][1], vertices[2][2]);
+        //
+        glColor3f(1.0f, 0.0f, 1.0f);     // Green
+        glVertex3f(vertices[2][0], vertices[2][1], vertices[2][2]);
+        glVertex3f(vertices[3][0], vertices[3][1], vertices[3][2]);
+        glVertex3f(vertices[7][0], vertices[7][1], vertices[7][2]);
+        glVertex3f(vertices[6][0], vertices[6][1], vertices[6][2]);
+        //
+        glColor3f(1.0f, 1.0f, 0.0f);     // Green
+        glVertex3f(vertices[0][0], vertices[0][1], vertices[0][2]);
+        glVertex3f(vertices[4][0], vertices[4][1], vertices[4][2]);
+        glVertex3f(vertices[7][0], vertices[7][1], vertices[7][2]);
+        glVertex3f(vertices[3][0], vertices[3][1], vertices[3][2]);
+        //
+        glColor3f(1.0f, 1.0f, 1.0f);     // Green
+        glVertex3f(vertices[4][0], vertices[4][1], vertices[4][2]);
+        glVertex3f(vertices[5][0], vertices[5][1], vertices[5][2]);
+        glVertex3f(vertices[6][0], vertices[6][1], vertices[6][2]);
+        glVertex3f(vertices[7][0], vertices[7][1], vertices[7][2]);
+    glEnd();
 }
--- src/DrawingUtilities.h
+++ src/DrawingUtilities.h
@@ -1,9 +1,12 @@
 #ifndef DrawingUtilities
 #define DrawingUtilities
 
+#include <GL/gl.h>
 #include <GL/glut.h>
+#include <vector>
 #include "globalVariables.h"
+using VectorOfVertices = std::vector<std::vector<GLfloat>>;
 
-void drawCube(GLfloat  x, GLfloat y, GLfloat  z);
+void drawCube(GLfloat height, GLfloat width, GLfloat depth, GLfloat  xc, GLfloat yc, GLfloat  zc);
 
 #endif
